e5a968b6e06003f387cac5385b94219dfb3b2147
Jarrett Bariel <jarrett.bariel@galvanize.com>
2020-05-14 00:57:07 +0000

    we have a canary

---
+++ src/main/java/com/galvanize/springbootmavendockerized/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.galvanize.springbootmavendockerized;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloWorldController {
+
+    @GetMapping("coal-mine")
+    public String canaryIsInTheCoalMine() {
+        return "Tweet";
+    }
+}

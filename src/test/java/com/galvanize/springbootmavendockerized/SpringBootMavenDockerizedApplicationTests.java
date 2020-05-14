f279d26b4ccdf490272ac517c2addb5eb7904f1a
Jarrett Bariel <jarrett.bariel@galvanize.com>
2020-05-14 00:49:24 +0000

    raw project => first commit

---
+++ src/test/java/com/galvanize/springbootmavendockerized/SpringBootMavenDockerizedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.springbootmavendockerized;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMavenDockerizedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

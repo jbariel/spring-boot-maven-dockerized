1d706888dccd57661929f386ed750c1fc47fcdc1
Jarrett Bariel <jarrett.bariel@galvanize.com>
2020-05-16 01:08:15 +0000

    refactor out tmpObject into an actual object

--- src/main/java/com/galvanize/springbootmavendockerized/HelloWorldController.java
+++ src/main/java/com/galvanize/springbootmavendockerized/HelloWorldController.java
@@ -1,10 +1,5 @@
 package com.galvanize.springbootmavendockerized;
 
-import java.io.Serializable;
-
-import javax.websocket.server.PathParam;
-
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,46 +15,13 @@ public class HelloWorldController {
     }
 
     @GetMapping("hello")
-    public TmpObject getJsonHelloObject() {
-        return new TmpObject();
+    public HelloWorldObject getJsonHelloObject() {
+        return new HelloWorldObject();
     }
 
     @GetMapping("hello/{user}")
-    public TmpObject getJsonHelloObject(@PathVariable String user) {
-        return new TmpObject(user);
+    public HelloWorldObject getJsonHelloObject(@PathVariable String user) {
+        return new HelloWorldObject(user);
     }
 
-    class TmpObject implements Serializable {
-        private static final long serialVersionUID = 1L;
-
-        public TmpObject() {
-            super();
-        }
-
-        public TmpObject(String name) {
-            this();
-            this.name = name;
-        }
-
-        private String value = "Hello %s!";
-
-        private String name = "World";
-
-        public String getValue() {
-            return String.format(value, name);
-        }
-
-        public void setValue(String value) {
-            this.value = value;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-    }
 }
